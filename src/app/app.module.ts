a835740150f440452af0b05e3b614951a1326b05
Dilyen <davidandohacquah@gmail.com>
2019-12-13 09:31:47 +0000

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RetweetComponent } from './retweet/retweet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RetweetComponent
   ],
   imports: [
     BrowserModule
